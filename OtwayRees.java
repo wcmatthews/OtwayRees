6988859208cc9beab3b020a0a27f88fcd6977105
Walter Matthews <collin.matthews@yahoo.com>
2018-11-13 10:37:55 -0600

    Method skeletons

--- OtwayRees.java
+++ OtwayRees.java
@@ -1,5 +1,25 @@
 public class OtwayRees {
+
+  static class Alice {
+    public void sendMessage() {
+
+    }
+  }
+
+  static class Bob {
+    public void sendMessage() {
+
+    }
+  }
+
+  static class Cathy {
+    public void sendKey() {
+        
+    }
+
+  }
+
   public static void main(String[] args) {
-  
+
   }
 }
